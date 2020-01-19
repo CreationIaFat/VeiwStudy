38a093405c30871fcc99dabd8630b6cd3dbc7a81
Creation Is Fat <freestyle.baller.sk8er@gmail.com>
2020-01-19 22:42:52 +0900

    3.2章を遊んでみたよ

---
+++ Vue.3.2/Vue3.2.vue
@@ -0,0 +1,14 @@
+<div id="fruts-list">
+<h1>フルーツ一覧</h1>
+<script src="https:unpkg.com/vue@2.5.17"></script>
+<div id="#fruts-list">
+    <fruts-list-title></fruts-list-title>
+</div>
+<script>
+Vue.compornent('fruts-list-title',{
+    template:'<h1>フルーツ一覧</h1>'
+})
+new Vue({
+    el:'#fruts-list'
+})
+</script>
